492c0b97c06040dc28fd1b0c44ef367cb1cd7a92
nickyfr33ze <nickyfr33ze26@pm.me>
2024-05-01 11:39:09 -0700

    Improved the script

--- script.py
+++ script.py
@@ -8,11 +8,11 @@ cwd = os.getcwd()
 print(f"Current working directory: {cwd}")
 
 file_name = input("What is the name of the file you want to create? ")
-print("Creating file...")
+print("[*] Creating file...")
 file_path = cwd + "/" + file_name + ".md"   # Create an empty file
 time.sleep(2)
-print(f"Created file: {file_path}")
-print("Creating the structure of the file...")
+print(f"[*] Created file: {file_path}")
+print("[*] Creating the structure of the file...")
 
 with open(file_path, "w") as file:
     file.write("# Question\n")
@@ -40,4 +40,11 @@ with open(file_path, "w") as file:
     file.close()
 
 time.sleep(2)
-print("Finished writing to file.")
+print("[*] Finished writing to file.")
+time.sleep(2)
+print("--------------------------------------------------------")
+time.sleep(1)
+print("[*] Please open the file '{}.md'' to begin.".format(file_name))
+time.sleep(2)
+print("[!] Exiting.")
+time.sleep(1)
